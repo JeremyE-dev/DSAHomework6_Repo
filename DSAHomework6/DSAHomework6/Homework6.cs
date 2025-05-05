1f4f129404841ee8b4304c7241508d61bd06d876
JeremyE-Dev <eudaly_19@hotmail.com>
2025-05-05 17:19:19 -0500

    add pre order traversal method

---
+++ DSAHomework6/DSAHomework6/Homework6.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAHomework6
+{
+ 
+    public class PrintVisitor<T> : Visitor<T>
+    {
+        public void Visit(T data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+
+    class Homework6
+    {
+        //a.) returns number of leaf nodes in the tree
+        //public static int CountLeaves(BinaryTree tree)
+        //{
+
+        //}
+
+        // b.) Returns number of non-leaf nodes in the tree
+        // public static int CountNonLeaves(BinaryTree tree)
+
+        // c.) Returns the height of the tree
+        // public static int Height(BinaryTree tree)
+
+        // d.) Prints the elements of the tree using a pre-order traversal
+        public static void PrintPreOrder(BinaryTree<int> tree)
+        {
+            PrintVisitor<int> visitor = new PrintVisitor<int>();
+            tree.TraversePreOrder(visitor);
+
+        }
+
+        // e.) Prints the elements of the tree using a in-order traversal
+        // public static void PrintInOrder(BinaryTree tree)
+
+        // f.) Prints the elements of the tree using a post-order traversal`
+        // public static void PrintPostOrder(BinaryTree tree)
+
+        // g.) Removes	all	leaf	nodes	from	the	tree. Use	PrintPreOrder,	
+        // PrintInOrder,	or PrintPostOrder  after calling RemoveLeaves to  show
+        // that RemoveLeaves successfully    removed all leaves.
+
+        // public static void RemoveLeaves(BinaryTree tree)
+
+
+        // Helpers
+
+        // traverse preorder
+        private static void TraversePreOrderRecursive<T>(
+            BinaryTree<T>.Node node,
+            Visitor<T> visitor)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            visitor.Visit(node.Data);
+            TraversePreOrderRecursive(node.Left, visitor);
+            TraversePreOrderRecursive(node.Right, visitor);
+        }
+
+    }
+}
--- DSAHomework6/DSAHomework6/Program.cs
+++ DSAHomework6/DSAHomework6/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace DSAHomework6
+﻿using System.Security.AccessControl;
+
+namespace DSAHomework6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+                          
+            BinaryTree<int> tree1 = new BinaryTree<int>();
+            // Root
+            var t1_root = tree1.InsertRoot(1);
+
+            // Left subtree of root
+            var t1_node2 = t1_root.InsertLeft(2);
+            var t1_node4 = t1_node2.InsertLeft(4);
+            var t1_node7 = t1_node4.InsertLeft(7);
+
+            // Right subtree of root
+            var t1_node3 = t1_root.InsertRight(3);
+            var t1_node5 = t1_node3.InsertLeft(5);
+            var t1_node6 = t1_node3.InsertRight(6);
+            var t1_node8 = t1_node6.InsertRight(8);
+            var t1_node9 = t1_node8.InsertRight(9);
+
+            // Print tree1 using pre-order traversal
+            Console.WriteLine();
+            Console.WriteLine("Tree 1 Pre-order");
+            Console.WriteLine("_________________________");
+            Homework6.PrintPreOrder(tree1);
+
+            // Create a second binary tree
+            BinaryTree<int> tree2 = new BinaryTree<int>();
+       
+            // Root is 6
+            var t2_root = tree2.InsertRoot(6);
+
+            // Left subtree
+            var t2_node4 = t2_root.InsertLeft(4);
+            var t2_node2 = t2_node4.InsertLeft(2);
+            t2_node4.InsertRight(5);
+            t2_node2.InsertLeft(1);
+            t2_node2.InsertRight(3);
+
+            // Right subtree
+            var t2_node8 = t2_root.InsertRight(8);
+            t2_node8.InsertLeft(7);
+            t2_node8.InsertRight(9);
+
+            // Print tree1 using pre-order traversal
+            Console.WriteLine();
+            Console.WriteLine("Tree 2 Pre-order");
+            Console.WriteLine("_______________________________");
+            Homework6.PrintPreOrder(tree2);
+
         }
     }
 }
